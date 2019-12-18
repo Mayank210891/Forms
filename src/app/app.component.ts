ca04cb348aa013ec458716c45eca4fa2b3dc1786
mbisht <mayank.bisht@greenwayhealth.com>
2019-12-18 09:25:53 +0530

    Template Driven Over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form',{static:false}) signUpForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   username: 'Mayank',
+    //   email: 'mayankbisht21@yahoo.com',
+    //   gender: 'Male',
+    //   secret: 'pet',
+    // })
+
+    this.signUpForm.form.patchValue({
+      username: 'Mayank Bisht'
+    })
   }
+  genderOptions=['Male', 'Female'];
+  
 }
